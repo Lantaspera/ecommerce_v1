816908e80ce71b940e358473355a681653850a0b
Justiinn <justin.mi20@iiitmk.ac.in>
2023-02-13 10:50:30 +0530

    node updated

---
+++ Server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app=express()
+mongoose.connect("mongodb://127.0.0.1:27017/newdb",{
+    useNewUrlParser:true, useUnifiedTopology:true
+},(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }else{
+     
+        console.log("successfully connected")   
+    }
+})
+
+app.listen(3006,()=>{
+    console.log("on port 3006 !!!")
+})
